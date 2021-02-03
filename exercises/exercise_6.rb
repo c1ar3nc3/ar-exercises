85c3f47b60f6a8c542a3679254ec23ffbc36eb37
Clarence Williams <xwilliams.clarencex@gmail.com>
2021-02-03 01:20:39 +0000

    Add Validation for new stores/employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,12 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Clarence", last_name: "Williams", hourly_rate: 200)
+@store1.employees.create(first_name: "Sepehr", last_name: "Sobani", hourly_rate: 999)
+@store1.employees.create(first_name: "Denys", last_name: "Pyshniuk", hourly_rate: 999)
+
+@store2.employees.create(first_name: "Bob", last_name: "Bie", hourly_rate: 60)
+@store2.employees.create(first_name: "Bill", last_name: "Lie", hourly_rate: 60)
+@store2.employees.create(first_name: "Jane", last_name: "Ded", hourly_rate: 60)
+@store2.employees.create(first_name: "Jill", last_name: "Ian", hourly_rate: 60)
